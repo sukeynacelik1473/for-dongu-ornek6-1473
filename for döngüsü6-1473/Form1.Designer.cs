d79c1201ac93810c31644f2d1bfee1565f0483b8
sukeynacelik1473 <145451091+sukeynacelik1473@users.noreply.github.com>
2023-10-23 16:00:42 +0300

    Add files via upload

---
+++ for döngüsü6-1473/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace for_döngüsü6_1473
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnYaz = new System.Windows.Forms.Button();
+            this.lbTek = new System.Windows.Forms.ListBox();
+            this.lbÇift = new System.Windows.Forms.ListBox();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnYaz
+            // 
+            this.btnYaz.Location = new System.Drawing.Point(12, 252);
+            this.btnYaz.Name = "btnYaz";
+            this.btnYaz.Size = new System.Drawing.Size(137, 64);
+            this.btnYaz.TabIndex = 0;
+            this.btnYaz.Text = "YAZ";
+            this.btnYaz.UseVisualStyleBackColor = true;
+            this.btnYaz.Click += new System.EventHandler(this.btnYaz_Click);
+            // 
+            // lbTek
+            // 
+            this.lbTek.FormattingEnabled = true;
+            this.lbTek.Location = new System.Drawing.Point(169, 65);
+            this.lbTek.Name = "lbTek";
+            this.lbTek.Size = new System.Drawing.Size(156, 251);
+            this.lbTek.TabIndex = 1;
+            // 
+            // lbÇift
+            // 
+            this.lbÇift.FormattingEnabled = true;
+            this.lbÇift.Location = new System.Drawing.Point(349, 65);
+            this.lbÇift.Name = "lbÇift";
+            this.lbÇift.Size = new System.Drawing.Size(160, 251);
+            this.lbÇift.TabIndex = 2;
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(12, 84);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(137, 20);
+            this.txtSayi1.TabIndex = 3;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Location = new System.Drawing.Point(15, 182);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(137, 20);
+            this.txtSayi2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "BİTİŞ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "BAŞLAGİÇ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.lbÇift);
+            this.Controls.Add(this.lbTek);
+            this.Controls.Add(this.btnYaz);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnYaz;
+        private System.Windows.Forms.ListBox lbTek;
+        private System.Windows.Forms.ListBox lbÇift;
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ for döngüsü6-1473/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace for_döngüsü6_1473
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnYaz_Click(object sender, EventArgs e)
+        {
+           
+          
+
+            int sayi1 = Convert.ToInt32(txtSayi1.Text);
+            int sayi2= Convert.ToInt32(txtSayi2.Text);
+            for (int i = sayi1; i <= sayi2; i++)
+
+            if (i % 2 == 0) 
+            {
+                lbÇift.Items.Add(i);
+
+            }
+            else
+            {
+                lbTek.Items.Add(i);
+            }
+
+
+            
+            
+
+            
+        }
+    }
+}
